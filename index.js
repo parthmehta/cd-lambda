a95bcb6c9381affdaac71e9cf4918f10f5040321
Parth <parth.rmehta@gmail.com>
2017-05-10 20:28:46 +0530

    Promises in parallel issue

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ log4js.configure({
 });
 
 //TODO
-const folderPath = "./tmp/"
+const folderPath = "tmp"
 
 function getSNSMessageObject(msgString) {
   var x = msgString.replace(/\\/g, '');
@@ -64,79 +64,94 @@ var test = exports.handler = function(event, context) {
   //TODO
   if (branchName && gitHubEvent.repository.action == "merged") {
 
-    let pathTofolder = path.resolve(__dirname, folderPath + appName)
+    let pathTofolder = path.resolve(__dirname, folderPath, appName)
 
-    let openRepo = new Promise((resolve, reject) => {
-      return NodeGit.Repository.open(pathTofolder).then((repo) => {
-        logger.info('OPEN_REPO', "SUCCESS", repo);
-        resolve(repo);
-      }).catch((errOpen) => {
-        logger.error('OPEN_REPO', "FAILED", errResult);
-        //TODO
-        Git.Clone(gitHubEvent.cloneURl, pathTofolder).then(function(repository) {
-          logger.error('CLONE_REPO', "SUCCESS", repository);
-          resolve(repository)
-        }).catch((errClone) => {
-          logger.info("CLONE_REPO", "FAILED", errClone)
-          reject(errClone)
-        });
-      })
-    })
-
-    let pullBranch = new Promise((resolve, reject) => {
-
-      NodeGit.Repository.open(pathTofolder).catch((errResult) => {
-        logger.error("PULL_BRANCH", "OPEN_FAILED", errResult)
-        return
-      }).then((repo) => {
-        logger.info('PULL_BRANCH', "OPEN SUCCESS", repo);
-        // TODO GIT CHECKOUT
-        return repo.getBranch('refs/remotes/origin/' + branchName).then(function(reference) {
-          //checkout branch
-          return repo.checkoutRef(reference).then((checkoutRef) => {
-
-            return repo.fetch("origin/" + branchName, {
+    let openRepo = function() {
+      return new Promise((resolve, reject) => {
+        NodeGit.Repository.open(pathTofolder).then((repo) => {
+          logger.info('OPEN_REPO', "SUCCESS", repo);
+          resolve(repo);
+        }).catch((errOpen) => {
+          logger.error('OPEN_REPO', "FAILED", errOpen);
+          //TODO
+          let fetchOptions = {
+            fetchOpts: {
               callbacks: {
                 certificateCheck: function() {
                   return 1;
                 }
               }
-            }).catch((errFetch) => {}).then(function(fetches) {
-              // Now that we're finished fetching, go ahead and merge
-              //TODO
-              // var signature = NodeGit.Signature.now("TableauDeploymentTool", "ab@c.de");
-              return repository.mergeBranches(branchName, "origin/" + branchName, null, null, {fileFavor: NodeGit.Merge.FILE_FAVOR.THEIRS});
-            })
-          })
+            }
+          }
 
-        });
-
+          NodeGit.Clone(cloneURL, path.resolve(__dirname, folderPath, appName), fetchOptions).then(function(repository) {
+            logger.info('CLONE_REPO', "SUCCESS", repository);
+            resolve(repository)
+          }).catch((errClone) => {
+            logger.info("CLONE_REPO", "FAILED", errClone)
+            reject(errClone)
+          });
+        })
       })
-    })
+    }
+
+    let pullBranch = function() {
+      return new Promise((resolve, reject) => {
+        NodeGit.Repository.open(pathTofolder).catch((errResult) => {
+          logger.error("PULL_BRANCH", "OPEN_FAILED", errResult)
+          return
+        }).then((repo) => {
+          logger.info('PULL_BRANCH', "OPEN SUCCESS", repo);
+          // TODO GIT CHECKOUT
+          return repo.getBranch('refs/remotes/origin/' + branchName).then(function(reference) {
+            //checkout branch
+            return repo.checkoutRef(reference).then((checkoutRef) => {
+
+              return repo.fetch("origin/" + branchName, {
+                callbacks: {
+                  certificateCheck: function() {
+                    return 1;
+                  }
+                }
+              }).catch((errFetch) => {}).then(function(fetches) {
+                // Now that we're finished fetching, go ahead and merge
+                //TODO
+                // var signature = NodeGit.Signature.now("TableauDeploymentTool", "ab@c.de");
+                return repository.mergeBranches(branchName, "origin/" + branchName, null, null, {fileFavor: NodeGit.Merge.FILE_FAVOR.THEIRS});
+              })
+            })
+
+          });
+
+        })
+      })
+    }
 
     //TODO
-    let deployCode = new Promise((resolve, reject) => {
-      let cmd = 'cd ' + folderPath + "/" + appName + ' && serverless deploy';
-      exec(cmd, function(error, stdout, stderr) {
-        // command output is in stdout
-        if (!error) {
-          // logmailer.info("DEPLOYED", "SUCCESS", {
-          //   appName: appName,
-          //   branchName: branchName
-          // })
-          resolve("successfully deplyed", stdout)
-        } else {
-          // logmailer.info("DEPLOYED", "FAILED", {
-          //   appName: appName,
-          //   branchName: branchName
-          // })
-          reject("Error occered", error, stderr)
-        }
+    let deployCode = function() {
+      return new Promise((resolve, reject) => {
+        let cmd = 'cd ' + folderPath + "/" + appName + ' && serverless deploy';
+        exec(cmd, function(error, stdout, stderr) {
+          // command output is in stdout
+          if (!error) {
+            // logmailer.info("DEPLOYED", "SUCCESS", {
+            //   appName: appName,
+            //   branchName: branchName
+            // })
+            resolve("successfully deplyed", stdout)
+          } else {
+            // logmailer.info("DEPLOYED", "FAILED", {
+            //   appName: appName,
+            //   branchName: branchName
+            // })
+            reject("Error occered", error, stderr)
+          }
+        })
       })
-    })
+    }
 
     //TODO Notify Status
-    Promise.all([openRepo]).then((data) => console.log(data)).catch((err) => console.log(err))
+    Promise.all([openRepo(), pullBranch(), deployCode()]).then((data) => console.log(data)).catch((err) => console.log(err))
 
   }
 }
